858b63b90030a90eb6c6c1782e1f7384f24660d4
Texmeijin <i8858garden@gmail.com>
2019-12-02 17:45:59 +0900

    vue-table-2を使ったけど行き詰まってきた

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,59 +1,72 @@
-const environment = process.env.NODE_ENV || 'development'
+const environment = process.env.NODE_ENV || 'development';
 const env = require(`./env.${environment}.js`)
 
 export default {
   env,
   mode: 'spa',
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
     title: process.env.npm_package_name || '',
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      {
+        charset: 'utf-8'
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1'
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: process.env.npm_package_description || ''
+      }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      {
+        rel: 'icon',
+        type: 'image/x-icon',
+        href: '/favicon.ico'
+      }
     ]
   },
   /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#fff' },
+   ** Customize the progress-bar color
+   */
+  loading: {
+    color: '#fff'
+  },
   /*
-  ** Global CSS
-  */
-  css: ['@/assets/css/main.scss'],
+   ** Global CSS
+   */
+  css: ['@/assets/css/main.scss', { src: 'bulma/bulma.sass', lang: 'sass' }],
   /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: ['@/plugins/firebase', '@/plugins/auth.ts'],
+   ** Plugins to load before mounting the App
+   */
+  plugins: [
+    '@/plugins/firebase',
+    '@/plugins/auth.ts',
+    '@/plugins/vue-tables-2'
+  ],
   /*
-  ** Nuxt.js dev-modules
-  */
+   ** Nuxt.js dev-modules
+   */
   buildModules: ['@nuxt/typescript-build'],
   /*
-  ** Nuxt.js modules
-  */
-  modules: [
-    '@nuxtjs/pwa',
-    '@nuxtjs/style-resources'
-  ],
+   ** Nuxt.js modules
+   */
+  modules: ['@nuxtjs/pwa', '@nuxtjs/style-resources'],
   styleResources: {
-    scss: [
-      '~/assets/css/main.scss'
-    ]
+    scss: ['~/assets/css/main.scss']
   },
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
     /*
-    ** You can extend webpack config here
-    */
-    extend (config, ctx) {
-    }
+     ** You can extend webpack config here
+     */
+    extend (config, ctx) {}
   }
 }
--- pages/post.vue
+++ pages/post.vue
@@ -1,17 +1,186 @@
 <template lang="pug">
-div 投稿画面
+main.sec-main
+  section.heading
+    h1.heading__head 比較をはじめる
+  section.dataTable
+    v-client-table(
+      v-model="tableData"
+      :columns="tableColumns"
+      :options="tableOptions"
+    )
+      template(v-if="products.length > 0")
+        template(
+          v-for="product in products"
+        )
+          input(
+            type="text"
+            :slot="`${product.id}`"
+            slot-scope="{row, update}"
+            v-model="row[`${product.id}`]"
+            @input="update"
+            class="--stringField"
+          )
+      //- input(
+      //-   type="text"
+      //-   :slot="`FIRST`"
+      //-   slot-scope="{row, update}"
+      //-   v-model="row[`FIRST`]"
+      //-   @input="update"
+      //-   class="--stringField"
+      //- )
+      //- input(
+      //-   type="text"
+      //-   :slot="`SECOND`"
+      //-   slot-scope="{row, update}"
+      //-   v-model="row[`SECOND`]"
+      //-   @input="update"
+      //-   class="--stringField"
+      //- )
+      button.delete(
+        type="button"
+        slot="remove"
+        slot-scope="{row}"
+        @click="remove(row.id)"
+      )
 </template>
 
 <script lang="ts">
-import { Vue, Component } from 'vue-property-decorator'
-import { namespace, Action } from 'vuex-class'
+import { Vue, Component } from 'vue-property-decorator';
+import { namespace, Action } from 'vuex-class';
 
-import * as auth from '~/store/auth'
+import * as auth from '~/store/auth';
 const Auth = namespace(auth.name)
 
-@Component({
-  middleware: 'auth'
-})
+enum InputType {
+  StringField,
+  StringArea,
+  Url,
+  Select
+}
+
+@Component
 export default class Post extends Vue {
+  compares: Array<any> = [];
+  products: Array<any> = [];
+
+  public created (): void {
+    this.products = [
+      {
+        id: 'FIRST',
+        name: '比較製品１'
+      },
+      {
+        id: 'SECOND',
+        name: '比較製品２'
+      }
+    ]
+    this.compares = [
+      {
+        meta: {
+          name: 'メーカー',
+          type: InputType.StringField
+        },
+        values: {
+          FIRST: 'MSI',
+          SECOND: 'IO-data'
+        }
+      },
+      {
+        meta: {
+          name: '価格',
+          type: InputType.StringField
+        },
+        values: {
+          FIRST: '10,000',
+          SECOND: '20,000'
+        }
+      }
+    ]
+  }
+
+  public get tableData (): Array<any> {
+    if (this.compares.length === 0) {
+      return []
+    }
+    console.log(
+      this.compares.map((data, index) => {
+        return {
+          checkPoint: data.meta.name,
+          ...data.values
+        }
+      })
+    )
+    return this.compares.map((data, index) => {
+      return {
+        checkPoint: data.meta.name,
+        ...data.values
+      }
+    })
+  }
+  public set tableData (newTableData: Array<any>) {
+    console.log(newTableData)
+  }
+  public get tableColumns (): Array<any> {
+    if (this.products.length === 0) {
+      return []
+    }
+    const columns = ['checkPoint']
+    console.log(columns.concat(this.products.map(el => el.id)))
+    return columns.concat(this.products.map(el => el.id))
+  }
+  public get tableOptions () {
+    const headings = {}
+    this.products.forEach((product) => {
+      headings[product.id] = product.name
+    })
+    const editableColumns = this.products.map((product) => {
+      return product.id
+    })
+    console.log(editableColumns)
+    return {
+      headings: {
+        checkPoint: '',
+        ...headings
+      },
+      filterable: false,
+      editableColumns
+    }
+  }
+  remove (id: string) {
+    console.log(id)
+    this.products = this.products.filter((product) => {
+      return product.id !== id
+    })
+  }
 }
 </script>
+
+<style lang="scss" scoped>
+.sec-main {
+  background: linear-gradient(#20e5f3, #089dca) fixed;
+  color: $white;
+  padding: 40px 0;
+  min-height: 100vh;
+
+  .heading {
+    padding: 0 12px;
+
+    .heading__head {
+      font-weight: bold;
+      font-size: 1.4rem;
+    }
+  }
+
+  .dataTable {
+    padding: 0 12px;
+    overflow-x: scroll;
+  }
+}
+
+.--stringField {
+  border: 1px solid $gray-light-3;
+  padding: 8px;
+  font-size: 1rem;
+  border-radius: 4px;
+}
+</style>
