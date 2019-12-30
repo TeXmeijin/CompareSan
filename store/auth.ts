c280d4d2c5aaaadd828105a5b358a5697a99e566
Texmeijin <i8858garden@gmail.com>
2019-12-30 16:40:21 +0900

    importを多少書き換え

--- store/auth.ts
+++ store/auth.ts
@@ -1,76 +1,74 @@
-import firebase from 'firebase';
-import pick from 'lodash/pick';
-import {
-  ActionTree,
-  MutationTree,
-} from 'vuex';
+import * as firebase from 'firebase/app'
+import 'firebase/auth'
+import pick from 'lodash/pick'
+import { ActionTree, MutationTree } from 'vuex'
 
-interface RootState { }
+interface RootState {}
 
 interface AuthState {
-  user: firebase.UserInfo | null;
-  uid: string | null;
-  credential: any | null;
+  user: firebase.UserInfo | null
+  uid: string | null
+  credential: any | null
 }
 
-export const name = 'auth';
+export const name = 'auth'
 
 export const state = (): AuthState => ({
   user: null,
   uid: null,
-  credential: null
-});
+  credential: null,
+})
 
 export const mutations: MutationTree<AuthState> = {
   setUser: (state: AuthState, user) => {
-    state.user = user;
+    state.user = user
   },
   setUid: (state: AuthState, uid: string) => {
-    state.uid = uid;
+    state.uid = uid
   },
   setCredential: (state: AuthState, credential: any) => {
     if (!credential || !state.uid) {
-      state.credential = null;
-      return;
+      state.credential = null
+      return
     }
     if (Object.keys(credential).length < 1) {
-      state.credential = null;
-      return;
+      state.credential = null
+      return
     }
-    state.credential = credential;
-  }
-};
+    state.credential = credential
+  },
+}
 
 export const actions: ActionTree<AuthState, RootState> = {
   login: () => {
-    const provider = new firebase.auth.TwitterAuthProvider();
-    firebase.auth().signInWithRedirect(provider);
+    const provider = new firebase.auth.TwitterAuthProvider()
+    firebase.auth().signInWithRedirect(provider)
   },
-  logout: ({
-    commit
-  }, uid: string) => {
-    firebase.auth().signOut();
+  logout: ({ commit }, uid: string) => {
+    firebase.auth().signOut()
 
-    commit('setUid', null);
-    commit('setUser', null);
-    commit('setCredential', null);
-    firebase.firestore().collection('users').doc(uid).set({});
+    commit('setUid', null)
+    commit('setUser', null)
+    commit('setCredential', null)
+    firebase
+      .firestore()
+      .collection('users')
+      .doc(uid)
+      .set({})
   },
-  setCredential: ({
-    commit
-  }, {
-    uid,
-    twitterId,
-    credential
-  }) => {
-    const db = firebase.firestore();
-    db.collection('users').doc(uid).set(Object.assign({
-      twitterId,
-      created_at: new Date().getTime()
-    }, pick(
-      credential,
-      ['accessToken', 'secret']
-    )));
-    commit('setCredential', credential);
-  }
-};
+  setCredential: ({ commit }, { uid, twitterId, credential }) => {
+    const db = firebase.firestore()
+    db.collection('users')
+      .doc(uid)
+      .set(
+        Object.assign(
+          {
+            twitterId,
+            created_at: new Date().getTime(),
+          },
+          pick(credential, ['accessToken', 'secret'])
+        )
+      )
+    commit('setCredential', credential)
+  },
+}
