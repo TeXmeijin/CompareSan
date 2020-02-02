ebec27dfc15aa8913dcd973c2b54b56ad6576250
Texmeijin <i8858garden@gmail.com>
2020-02-02 15:53:39 +0900

    デプロイ修正とヘッダーの幅修正

--- client/components/molecules/TableHeader.vue
+++ client/components/molecules/TableHeader.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-  .TableHeader
+  .TableHeader(:style="{ width: `${rowWidth}px` }")
     .emptyHeaderItem(:style="{ minWidth: headWidth }")
     .TableHeaderItem(v-for="header in tableHeader" :style="{ width: cellWidth }")
       comparing-item(
@@ -36,7 +36,14 @@ export default class TableHeaderVue extends Vue {
   tableHeader: TableHeader
 
   @TableSize.Getter headWidth
+  @TableSize.Getter headWidthRaw
   @TableSize.Getter cellWidth
+  @TableSize.Getter cellWidthRaw
+  @TableSize.Getter cellHeight
+
+  get rowWidth (): number {
+    return this.headWidthRaw + this.cellWidthRaw * this.tableHeader.length
+  }
 }
 </script>
 
