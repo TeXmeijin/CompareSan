8bd43a4410e6043057fc137aa876739168c9664b
Texmeijin <i8858garden@gmail.com>
2020-02-02 13:04:14 +0900

    ヘッダー幅も修正

--- client/components/molecules/ReadOnly/ReadOnlyTableHeader.vue
+++ client/components/molecules/ReadOnly/ReadOnlyTableHeader.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-  .ReadOnlyTableHeader
+  .ReadOnlyTableHeader(:style="{ width: `${rowWidth}px` }")
     .emptyHeaderItem(:style="{ minWidth: headWidth }")
     .TableHeaderItem(v-for="header in tableHeader" :style="{ width: cellWidth }")
       comparing-item(
@@ -31,7 +31,14 @@ export default class ReadOnlyTableHeaderVue extends Vue {
   tableHeader: TableHeader
 
   @TableSize.Getter headWidth
+  @TableSize.Getter headWidthRaw
   @TableSize.Getter cellWidth
+  @TableSize.Getter cellWidthRaw
+  @TableSize.Getter cellHeight
+
+  get rowWidth (): number {
+    return this.headWidthRaw + this.cellWidthRaw * this.tableHeader.length
+  }
 }
 </script>
 
